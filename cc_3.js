edec00e9bd8a3f35fac6b2f4d16f5e9144b3c144
Hieu Nguyen <hieuunguyenn979@gmail.com>
2024-05-17 23:39:28 -0400

    Add documentation

--- cc_3.js
+++ cc_3.js
@@ -2,23 +2,32 @@
 // U12840485
 
 class Car{
+    // Create the instances of the Car class with 'make' and 'speed' properties
     constructor (make, speed){
         this.make = make;
         this.speed = speed;       
     }
 
+    // Create the accelerate method 
     accelerate (){
+        // Increase the speed by 10 each time it is called
         this.speed += 10;
+
+        // Display the message
         console.log(`The speed of the ${this.make} after accelerating is ${this.speed} km/h.`);
     }
 
+    // Create the brake method 
     brake () {
+        // Decrease the speed by 5 each time it is called
         this.speed -= 5;
+
+        // Display the message
         console.log(`The speed of the ${this.make} after braking is ${this.speed} km/h.`);
     }
 }
 
-// Test Car 1:
+// Test Car 1: BWM  starting at 120 km/h
 let bmw = new Car('BMW', 120);
 console.log(`The car brand is ${bmw.make}.`);
 console.log(`The initial speed is ${bmw.speed} km/h.`);
@@ -30,7 +39,7 @@ bmw.accelerate();
 bmw.accelerate();
 
 console.log("----------------------------------------------------");
-// Test Car 2:
+// Test Car 2: Mercedes starting at 95 km/h
 let merc = new Car("Mercedes", 95); 
 console.log(`The car brand is ${merc.make}.`);
 console.log(`The initial speed is ${merc.speed} km/h.`);
