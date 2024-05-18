6373fa483adfb01e89969abcf601598264a1ef58
Hieu Nguyen <hieuunguyenn979@gmail.com>
2024-05-17 23:26:01 -0400

    Test accelerate and brake methods on Car instances.

--- cc_3.js
+++ cc_3.js
@@ -17,3 +17,26 @@ class Car{
         console.log(`The speed of the ${this.make} after braking is ${this.speed} km/h.`);
     }
 }
+
+// Test Car 1:
+let bmw = new Car('BMW', 120);
+console.log(`The car brand is ${bmw.make}.`);
+console.log(`The initial speed is ${bmw.speed} km/h.`);
+bmw.brake();
+bmw.accelerate();
+bmw.brake();
+bmw.brake();
+bmw.accelerate();
+bmw.accelerate();
+
+console.log("----------------------------------------------------");
+// Test Car 2:
+let merc = new Car("Mercedes", 95); 
+console.log(`The car brand is ${merc.make}.`);
+console.log(`The initial speed is ${merc.speed} km/h.`);
+merc.accelerate();
+merc.brake();
+merc.brake();
+merc.accelerate();
+merc.accelerate();
+
